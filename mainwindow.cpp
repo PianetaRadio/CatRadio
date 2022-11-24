7fb5e4ccc4b036ed5f86a53e2563b48fbbff0e65
PianetaRadio <78976006+PianetaRadio@users.noreply.github.com>
2022-11-24 09:35:03 +0100

    Dark theme about
    #33

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1471,12 +1471,12 @@ void MainWindow::on_action_AboutCatRadio_triggered()
     msgBox.setTextFormat(Qt::RichText);
     QString version = QString::number(VERSION_MAJ)+"."+QString::number(VERSION_MIN)+"."+QString::number(VERSION_MIC);
     msgBox.setText("<b>CatRadio</b> <i>Radio control software</i><br/>version "+version+" "+RELEASE_DATE);
-    msgBox.setInformativeText("Copyright (C) 2022 Gianfranco Sordetti IZ8EWD<br/>"
+    msgBox.setInformativeText("<p>Copyright (C) 2022 Gianfranco Sordetti IZ8EWD<br/>"
                               "<a href='https://www.pianetaradio.it'>www.pianetaradio.it</a></p>"
                               "<p>This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.<br/>"
                               "This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.<br/>"
-                              "You should have received a copy of the GNU General Public License along with this program.  If not, see <a href='http://www.gnu.org/licenses/'>www.gnu.org/licenses</a>.");
-    msgBox.setIcon(QMessageBox::NoIcon);
+                              "You should have received a copy of the GNU General Public License along with this program.  If not, see <a href='http://www.gnu.org/licenses/'>www.gnu.org/licenses</a>.</p>");
+    msgBox.setIcon(QMessageBox::Information);
     msgBox.setStandardButtons(QMessageBox::Ok);
 
     QPixmap icon("catradio.png");
@@ -1501,6 +1501,28 @@ void MainWindow::on_action_AboutHamLib_triggered()
     msgBox.setStandardButtons(QMessageBox::Ok);
     msgBox.exec();
 }
+
+/**
+void MainWindow::on_action_AboutDarkTheme_triggered()
+{
+    QMessageBox msgBox;
+    msgBox.setWindowTitle("About Dark Theme");
+    msgBox.setTextFormat(Qt::RichText);
+    msgBox.setText("<b>Dark Theme</b>);
+    msgBox.setInformativeText("<p>Copyright (C) ...<br/>"
+                              "<a href='https://...'>www....</a></p>"
+                              "<p>The MIT License (MIT)</p>
+                              "<p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:<br/>"
+                              "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.<br/>"
+                              "THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>");
+    msgBox.setIcon(QMessageBox::NoIcon);
+    msgBox.setStandardButtons(QMessageBox::Ok);
+
+    QPixmap icon("catradio.png");
+    msgBox.setIconPixmap(icon);
+
+    msgBox.exec();
+}**/
 
 void MainWindow::on_action_CatRadioHomepage_triggered()
 {
